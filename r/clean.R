882201be4c56be90b97214d30aeb348cc20a9c71
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-04 15:44:35 -0400

    separate the geocoding script

--- r/clean.R
+++ r/clean.R
@@ -8,11 +8,11 @@ gva1 <- read_csv(here("data", "2013-02-11 - 2017-03-18.csv"),
 gva2 <- read_csv(here("data", "2017-03-12 - 2021-10-27.csv"),
                  col_types = list("Incident Date" = col_datetime(format = "%B %d, %Y")))
 
-# combine two csvs
+# combine two csv files
 gva <- gva1 %>% bind_rows(gva2) %>% 
     arrange(`Incident Date`, `Incident ID`)
 
-# Get unduplicated data
+# Get unduplicated data (some incidents overlap because of the download procedure)
 gva_unique <- gva %>% distinct()
 
 # Rename columns
@@ -24,3 +24,18 @@ gva_renamed <- gva_unique %>%
            address = "Address",
            killed = "# Killed",
            injured = "# Injured")
+
+# Clean missing values in address: "N/A" to NA_character_
+gva_renamed <- gva_renamed %>%
+    mutate(address = str_replace(address, "N/A", NA_character_))
+
+# Save the incidents data without geocode
+write_rds(gva_renamed, here("data", "incidents_nogeocode.rds"))
+
+# Check if we already have the geocoded rds file
+geocoded_exists <- file.exists(here("data", "incidents.rds"))
+
+# Run the geocode R file (takes time)
+if(!geocoded_exists) source(here("r", "geocode.R"))
+cat(green("The geocoded file already exists in the data folder.\n
+          Not running the cleaning script."))
