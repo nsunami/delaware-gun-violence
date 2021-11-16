1ece7fcc218a941a9eae9630397b571e8855d869
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-16 14:02:51 -0500

    rename variable to incidents

--- r/geocode.R
+++ r/geocode.R
@@ -8,7 +8,7 @@ library(ggmap)
 library(tigris)
 
 # Get data without geocode
-gva_renamed <- read_rds(here("data", "incidents_nogeocode.rds"))
+incidents_raw <- read_rds(here("data", "incidents_nogeocode.rds"))
 
 # Google Map API to clean the address
 # Register with google 
@@ -18,19 +18,19 @@ gva_renamed_temp <- gva_renamed %>%
     head(100)
 
 # Create a string to feed to Google Map API
-gva_renamed_temp <- gva_renamed_temp %>%
+incidents_raw <- incidents_raw %>%
     mutate(full_address = paste(address, city_county, state, sep = ", "))
 
 # Query Google Map API  zzzzzzzz (takes time) zzzzzzzz
-gva_renamed_temp <- gva_renamed_temp %>%
+incidents_raw <- incidents_raw %>%
     mutate(lon_lat = geocode(full_address)) %>% 
         unnest(lon_lat) # unnest since the data returned is df
     
 # Add census blocks via tigris:: zzzzzzzz (takes time) zzzzzzzz
-incidents_geo_clean <- gva_renamed_temp %>% 
+incidents_clean <- incidents_raw %>% 
     mutate(census_block = map2_chr(lon, lat, 
                                .f = ~call_geolocator_latlon(lon = .x, lat = .y)))
 
 # Save incidents data
-write_rds(incidents_geo_clean, here("data", "incidents.rds"))
-write_csv(incidents_geo_clean, here("data", "incidents.csv"))
+write_rds(incidents_clean, here("data", "incidents.rds"))
+write_csv(incidents_clean, here("data", "incidents.csv"))
