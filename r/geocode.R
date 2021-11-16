036de58e623dc9cd88fc98d5f0e5f7d62735dc3a
Nami Sunami <naoyuki.sunami@gmail.com>
2021-11-16 14:03:17 -0500

    remove 100 limiter

--- r/geocode.R
+++ r/geocode.R
@@ -14,9 +14,6 @@ incidents_raw <- read_rds(here("data", "incidents_nogeocode.rds"))
 # Google Map API to clean the address
 # Register with google 
 register_google(key = Sys.getenv("GOOGLE_GEO_KEY"))
-# Limit the query to 100 for now
-gva_renamed_temp <- gva_renamed %>%
-    head(100)
 
 # Create a string to feed to Google Map API
 incidents_raw <- incidents_raw %>%
